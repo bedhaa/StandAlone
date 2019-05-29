63d7cd5f3288f9fb70bce3453749b59eaa0de4cb
Sehati - Bedha <Sehati - Bedha@LAPTOP-C1V6GTB0>
2019-05-29 11:05:25 +0700

    add Object Repository

---
+++ Test Suites/StandAlone.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>StandAlone</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1414b6fe-6271-4fd3-967e-69ce3685c283</testSuiteGuid>
+   <testCaseLink>
+      <guid>0110656c-afd3-48c3-8b83-e63670c69a32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SA_Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b57264b8-7c26-402b-ba0b-ef8d7c388de1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SA_Login2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
