6edf5daa80089ddc69813d54b7a89996b944d213
AdeliaIndah <adelia@telectg.co.id>
2019-06-13 11:01:50 +0700

    add testcase Pengaturan - Ubah Kata Sandi

---
+++ Scripts/Pengaturan/Ubah Kata Sandi - Password Tidak Boleh Kosong/Script1560396822568.groovy
@@ -0,0 +1,45 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('C:\\Users\\Sehati-Adel\\Downloads\\CTG-SA.apk', true)
+
+Mobile.setText(findTestObject('Ubah Kata Sandi/android.widget.EditText0 - User ID Anda'), 'lilies.pbnd', 0)
+
+Mobile.setText(findTestObject('Ubah Kata Sandi/android.widget.EditText1 - Kata Sandi Anda'), 'qwerty123456', 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.Button0 - Masuk'), 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.ImageView0'), 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.FrameLayout3'), 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.EditText0 - Kata sandi baru'), 0)
+
+Mobile.setText(findTestObject('Ubah Kata Sandi/android.widget.EditText0 - Kata sandi baru (1)'), '', 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.ImageButton0'), 0)
+
+Mobile.setText(findTestObject('Ubah Kata Sandi/android.widget.EditText1 - Konfirmasi kata sandi baru'), 'qwerty', 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.ImageButton1'), 0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.Button0 - Ubah Kata Sandi Saya'), 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.TextView0 - Kembali'), 0)
+
+Mobile.tap(findTestObject('Ubah Kata Sandi/android.widget.Button0 - Keluar'), 0)
+
